2123bd9538e3fd367abc9753aed9b884d57a22b2
JHON TORRES <samuelithotorres2001@gmail.com>
2026-02-10 17:01:16 +0000

    se agrego la suma de numero pares e impares al arreglo

--- Semana2/Ejercicio1.java
+++ Semana2/Ejercicio1.java
@@ -9,5 +9,16 @@ public class Ejercicio1 {
         for (int i = 0; i < a.length; i++) {
            System.out.println("a[" + i + "]"+a [i] ); 
         }
+
+        // suma los numero pares impares del arreglo y mostrar el resultado
+        int sumaPares = 0;  int sumaImpares = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a [i]%2==0) {
+                sumaPares + = a[i];
+            } else {
+                sumaImpares+=a[i];
+            }
+        }
+        System.out.println("suma Pares = " + sumaPares+ "suma impares" + sumaImpares );
     }
 }
