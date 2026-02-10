a49b87d2900d4a075d5a35b9dfa0702af003485b
JHON TORRES <samuelithotorres2001@gmail.com>
2026-02-10 16:30:18 +0000

    se agrego un numero al arreglo

--- Semana2/Ejercicio1.java
+++ Semana2/Ejercicio1.java
@@ -2,8 +2,9 @@ package Semana2;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
+        
         //forma 1 --> Definiendio directamente los valores del arreglo
-        int[]a={4,8,9,6,1,2};
+        int[]a={4,8,9,6,1,2,10};
         // iterar los elementos del arreglo
         for (int i = 0; i < a.length; i++) {
            System.out.println("a[" + i + "]"+a [i] ); 
