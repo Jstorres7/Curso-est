c2c9a55b850147cc3f551202fada9e731a1293c2
JHON TORRES <samuelithotorres2001@gmail.com>
2026-02-10 16:26:55 +0000

    Primer ejercicio Con Arreglos

---
+++ Semana2/Ejercicio1.java
@@ -0,0 +1,12 @@
+package Semana2;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //forma 1 --> Definiendio directamente los valores del arreglo
+        int[]a={4,8,9,6,1,2};
+        // iterar los elementos del arreglo
+        for (int i = 0; i < a.length; i++) {
+           System.out.println("a[" + i + "]"+a [i] ); 
+        }
+    }
+}
